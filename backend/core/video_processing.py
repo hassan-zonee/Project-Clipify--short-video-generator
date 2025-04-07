15e2f5958158a5bd968ac4a3e43e0db10a705990
hassan <mhassanofficial7@gmail.com>
2025-04-07 22:51:49 +0500

    working on clip generation

--- backend/core/video_processing.py
+++ backend/core/video_processing.py
@@ -2,23 +2,30 @@ import uuid
 import os
 from datetime import datetime
 import yt_dlp
+from moviepy import VideoFileClip
+import time
+
+video_download_path = "media/videos"
+generated_clips_path = "media/clips"
 
 def process_video(url):
-    download_video(url)
+    #downloaded_video = download_video(url)
+    downloaded_video = "media/videos/20250407_215320_33c2ff2e-1439-4fa2-a80c-658a11683c9d.mp4"
+    print("Downloaded Video: ", downloaded_video)
+    clips = split_video_into_clips(downloaded_video)
 
 def download_video(url: str):
-    """
-    Download the video from the provided URL and store it temporarily with a unique filename.
-    :param url: The URL of the video to download.
-    :return: None
-    """
-
     print('downloading...')
-    output_path = generate_unique_filename(extension=".mp4")
+    output_path = generate_unique_filename(video_download_path, extension=".mp4")
     ydl_opts = {
         'format': 'bestvideo[height<=480]+bestaudio/best',
         'outtmpl': output_path,
         'quiet': True,
+        'merge_output_format': 'mp4',
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4'
+        }]
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -26,6 +33,25 @@ def download_video(url: str):
 
     return output_path
 
+
+def split_video_into_clips(video_path, clip_duration=30):
+    video = VideoFileClip(video_path)
+    total_duration = int(video.duration)
+    print("duration: ", total_duration)
+
+    clip_paths = []
+
+    start_time = 0
+    while start_time < total_duration:
+        end_time = min(start_time + clip_duration, total_duration)
+        clip = video.subclipped(start_time, end_time)
+        output_path = generate_unique_filename(generated_clips_path, extension=".mp4")
+        clip.write_videofile(output_path, codec="libx264", audio_codec="aac", fps=24)
+        clip_paths.append(output_path)
+        start_time = end_time
+
+    video.close()
+    return clip_paths
 
 
 
@@ -37,15 +63,10 @@ def delete_file(file_path: str):
         print(f"File not found: {file_path}")
 
 
-def generate_unique_filename(extension=".mp4"):
-    """
-    Generate a unique filename for the video download using UUID and timestamp.
-    :param extension: File extension (e.g., '.mp4')
-    :return: Unique file name as a string
-    """
+def generate_unique_filename(path, extension=".mp4"):
     unique_id = str(uuid.uuid4())
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"{timestamp}_{unique_id}{extension}"
-    file_path = os.path.join("media/videos", filename)
+    file_path = os.path.join(path, filename)
 
     return file_path
